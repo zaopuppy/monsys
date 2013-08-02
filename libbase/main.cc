abdf22323fdf2dbabd696b2ae1c016e95660d888
Joey Zhao <zhaoyi.zero@gmail.com>
2013-08-02 23:48:50 +0800

    code refact

---
+++ libbase/main.cc
@@ -0,0 +1,12 @@
+#include "zlog.h"
+#include "zcodecpp.h"
+
+///////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+  Z_LOG_I("Just a test.\n");
+
+  return 0;
+}
+
---
+++ stub/main.cc
@@ -0,0 +1,50 @@
+// system api
+#include <iostream>
+#include <assert.h>
+
+#include <event2/event.h>
+
+#include "fgw_server.h"
+
+using namespace std;
+
+ZServer *g_server = NULL;
+
+static void routine(evutil_socket_t fd, short events, void *arg)
+{
+	g_server->routine(500);
+}
+
+int main(int argc, char *argv[])
+{
+	struct event_base* base = event_base_new();
+	assert(base);
+
+	g_server = new FGWServer("0.0.0.0", 1983, base);
+	if (OK != g_server->init()) {
+		Z_LOG_E("Failed to start FGW Server, quit\n");
+		return -1;
+	}
+
+	{
+		const struct timeval ROUTINE_INTERVAL = { 0, 500 * 1000 };
+		// struct event *timeout_ev = evtimer_new(base, routine, NULL);
+		// XXX: don't use EV_PEERSIST, time-cost routine may be dangerous
+		struct event *timeout_ev = event_new(base, -1, EV_PERSIST, routine, NULL);
+		event_add(timeout_ev, &ROUTINE_INTERVAL);
+	}
+
+	// basicly equals to event_base_loop()
+	// event_base_dispatch(base);
+	while (1) {
+		printf("beginning of loop\n");
+		// event_base_loop(base, EVLOOP_NONBLOCK);
+		event_base_loop(base, 0);
+		printf("end of loop\n");
+
+	}
+
+	return 0;
+}
+
+
