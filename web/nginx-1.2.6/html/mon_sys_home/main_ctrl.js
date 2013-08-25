2a1d91fff99f160e64ec3254f957de16e9e9ae3d
Joey Zhao <zhaoyi.zero@gmail.com>
2013-08-25 10:52:48 +0800

    web client modification

--- web/nginx-1.2.6/html/mon_sys_home/main_ctrl.js
+++ web/nginx-1.2.6/html/mon_sys_home/main_ctrl.js
@@ -35,6 +35,28 @@ function MainCtrl() {
 		$('#device-list').empty();
 	}
 
+	this.gen_record = function(dev_info) {
+		var record = $('<div></div>');
+
+		// icon
+		var img = this.get_icon_by_type(dev_info["type"]);
+		// button
+		var btn = $('<input type="button"></input>');
+		btn.val(dev_info["name"]);
+
+		// append them
+		record.append(img);
+		record.append(btn);
+		btn.click(function(addr, type) {
+			return function() {
+				console.log("operation on device: addr=" + addr + ", type=" + type);
+				do_opera_device(addr, type);
+			}
+		}(dev_info["addr"], dev_info["type"]));
+
+		return record;
+	}
+
 	this.update_list_box = function(dev_info_list) {
 
 		this.clear_device_list_box();
@@ -55,24 +77,45 @@ function MainCtrl() {
 			return;
 		}
 
-		// var tbl = $('<table id="device-table"></table>');
+		var device_list = $('#device-list');
+
+		for (var i = 0; i < devs.length; ++i) {
+			var record = this.gen_record(devs[i]);
+			device_list.append(record);
+		}
+
+	}
+
+	this.update_list_box_old = function(dev_info_list) {
+
+		this.clear_device_list_box();
+
+		// --- for debugging only ---
+		dev_info_list = {"cmd": "get-dev-list-rsp", "devs": [{"type": 0, "addr": 0, "name": "dev-00", "state": 0}, {"type": 1, "addr": 1, "name": "dev-01", "state": 1}, {"type": 2, "addr": 2, "name": "dev-02", "state": 2}, {"type": 3, "addr": 3, "name": "dev-03", "state": 3}, {"type": 4, "addr": 4, "name": "dev-04", "state": 4}], "status": 0};
+		// --- for debugging only ---
+
+		// refresh
+		// TODO: should do it at the very beginning
+		// if (jobj.hasOwnProperty("devs"))
+		// var tbl = $("#device-table");
+		var devs = dev_info_list["devs"];
+		if (devs.length <= 0) {
+			// append empty notice
+			var notice = $('<p>No device was found:(</p>');
+			$('#device-list').append(notice);
+			return;
+		}
+
 		var tbl = $('<ul id="device-table"></ul>');
 		for (var i = 0; i < devs.length; ++i) {
 			var row = $('<li class="device-record"></li>');
-			// var row = $('<tr class="device-record"></tr>');
 
 			// Image, name, desc, state, operate
-			// var imageCell = $('<td></td>');
-			// var nameCell = $('<td></td>');
-			// var descCell = $('<td></td>');
-			// var operaCell = $('<td></td>');
 			var imageCell = $('<span class="image"></span>');
 			var nameCell = $('<span class="name"></span>');
 
 			// image
-			// var img = document.createElement("img");
 			var img = this.get_icon_by_type(devs[i]["type"]);
-			// img.src = "Safari_Agua.png";
 			// can't use img's width, for async loading...
 			// imageCell.width(48);
 			imageCell.append(img);
